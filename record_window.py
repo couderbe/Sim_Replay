ce48b229669b601791d38b77a79370e00e83e7ee
Benoît Couderc <couderc.benoit@orange.fr>
2023-05-15 19:11:04 +0200

    Added comments and scrapped units from documentation

--- record_window.py
+++ record_window.py
@@ -71,13 +71,36 @@ class RecordWindow(QDialog):
 
 class RecorderListModel(QAbstractListModel):
     def __init__(self, data: list[dict], parent: QObject | None = ...) -> None:
+        """Custom ListModel for the record window
+
+        Args:
+            data (list[dict]): Initial data to be stored in the model. Each dict in the list shall have name and description keys defined.
+            parent (QObject | None, optional): Parent Object. Defaults to ....
+        """
         super().__init__(parent)
         self._data = data
 
     def rowCount(self, parent: QModelIndex | QPersistentModelIndex = ...) -> int:
+        """Return the number of rows in the model
+
+        Args:
+            parent (QModelIndex | QPersistentModelIndex, optional): parent. Defaults to ....
+
+        Returns:
+            int: row count
+        """
         return len(self._data)
 
-    def data(self, index: QModelIndex | QPersistentModelIndex, role: int = Qt.DisplayRole) -> str:
+    def data(self, index: QModelIndex | QPersistentModelIndex, role: int = Qt.DisplayRole) -> Any:
+        """Returns the model data at given index for a given row
+
+        Args:
+            index (QModelIndex | QPersistentModelIndex): index
+            role (int, optional): Qtrole. Defaults to Qt.DisplayRole.
+
+        Returns:
+            Any: data
+        """
         if index.isValid():
             match role:
                 case Qt.DisplayRole:
@@ -92,12 +115,30 @@ class RecorderListModel(QAbstractListModel):
         return super().headerData(section, orientation, role)
 
     def flags(self, index: QModelIndex | QPersistentModelIndex) -> Qt.ItemFlag:
-        if index.isValid(): # i.e item of the listView
+        """Return itemflag for the given index. An item of the listview can be dragged but it is not possible to drop an item on it whereas the listView itself can be dropped on but not dragged
+
+        Args:
+            index (QModelIndex | QPersistentModelIndex): index
+
+        Returns:
+            Qt.ItemFlag: flags
+        """
+        if index.isValid():  # i.e item of the listView
             return Qt.ItemFlag.ItemIsEnabled | Qt.ItemFlag.ItemIsSelectable | Qt.ItemFlag.ItemIsDragEnabled
-        else: # i.e listView itself
+        else:  # i.e listView itself
             return Qt.ItemFlag.ItemIsEnabled | Qt.ItemFlag.ItemIsSelectable | Qt.ItemFlag.ItemIsDropEnabled
 
     def removeRows(self, row: int, count: int, parent: QModelIndex | QPersistentModelIndex = ...) -> bool:
+        """Remove count of rows from row
+
+        Args:
+            row (int): row to start deletion from
+            count (int): number of rows to delete
+            parent (QModelIndex | QPersistentModelIndex, optional): parent. Defaults to ....
+
+        Returns:
+            bool: True if the operation is succesful. False otherwise
+        """
         self.beginRemoveRows(parent, row, row + count - 1)
         for i in range(row, row + count):
             self._data.pop(i)
@@ -105,6 +146,16 @@ class RecorderListModel(QAbstractListModel):
         return True
 
     def insertRows(self, row: int, count: int, parent: QModelIndex | QPersistentModelIndex = ...) -> bool:
+        """Insert count of rows from row
+
+        Args:
+            row (int): row to start insertion from
+            count (int): number of rows to add
+            parent (QModelIndex | QPersistentModelIndex, optional): parent. Defaults to ....
+
+        Returns:
+            bool: True if the operation is succesful. False otherwise
+        """
         self.beginInsertRows(parent, row, row + count - 1)
         for i in range(row, row + count):
             self._data.insert(i, {})
@@ -112,6 +163,16 @@ class RecorderListModel(QAbstractListModel):
         return True
 
     def setData(self, index: QModelIndex | QPersistentModelIndex, value: Any, role: int = ...) -> bool:
+        """Set raw data at given index.
+
+        Args:
+            index (QModelIndex | QPersistentModelIndex): index
+            value (Any): value to save
+            role (int, optional): role Defaults to ....
+
+        Returns:
+            bool: True if the operation is succesful. False otherwise
+        """
         if index.isValid():
             match role:
                 case UserRoles.RAW.value:
@@ -120,21 +181,56 @@ class RecorderListModel(QAbstractListModel):
         return False
 
     def supportedDragActions(self) -> Qt.DropAction:
+        """Set drag to move action
+
+        Returns:
+            Qt.DropAction: 
+        """
         return Qt.DropAction.MoveAction
 
     def supportedDropActions(self) -> Qt.DropAction:
+        """Set drop to move action
+
+        Returns:
+            Qt.DropAction: 
+        """
         return Qt.DropAction.MoveAction
 
     def mimeTypes(self) -> List[str]:
+        """json is the only supported mimetype from drag and drop actions.
+
+        Returns:
+            List[str]: 
+        """
         return ["application/json"]
 
     def mimeData(self, indexes: Sequence[QModelIndex]) -> QMimeData:
+        """Encode the raw data of the indexes into json
+
+        Args:
+            indexes (Sequence[QModelIndex]): indexes to encode
+
+        Returns:
+            QMimeData: encoded indexes' data
+        """
         mimeData = QMimeData()
         mimeData.setData("application/json", json.dumps(
             [index.data(int(UserRoles.RAW.value)) for index in indexes]).encode())
         return mimeData
 
     def dropMimeData(self, data: QMimeData, action: Qt.DropAction, row: int, column: int, parent: QModelIndex | QPersistentModelIndex) -> bool:
+        """Decode dropped elements and add them to the model. New rows are created at the drop position. If the list is empty the dropped position is manually set to 0.
+
+        Args:
+            data (QMimeData): dropped data
+            action (Qt.DropAction): action
+            row (int): row position of the drop action
+            column (int): column position of the drop action. Unused in this model type.
+            parent (QModelIndex | QPersistentModelIndex): parent
+
+        Returns:
+            bool: True if the operation is succesful. False otherwise
+        """
         # If list is empty dropped row value is -1
         if row == -1:
             row = 0
